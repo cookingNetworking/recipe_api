922a64d0bd9e4cf0f9da65ec00efc6678e5692eb
as2229181 <as2229181@gmail.com>
2023-10-23 00:06:01 +0800

    add csrfmixin

--- app/recipe/utils.py
+++ app/recipe/utils.py
@@ -1,7 +1,11 @@
 import boto3
-from django.conf import settings
+
 from botocore.config import Config
 
+from django.views.decorators.csrf import csrf_protect
+from django.conf import settings
+
+from rest_framework.viewsets import ModelViewSet
 def generate_presigned_url(bucket_name, object_name, expiration=3600):
     """Generate a presigned URL to share an S3 object."""
     s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
@@ -14,4 +18,14 @@ def generate_presigned_url(bucket_name, object_name, expiration=3600):
                                                 Params={'Bucket': bucket_name,
                                                         'Key': object_name},
                                                 ExpiresIn=expiration)
+
     return response
+
+
+
+class UnsafeMethodCSRFMixin(ModelViewSet):
+    """Amixin that applies CSRF protection to unsafe HTTP methods (POST, PATCH, PUT, DELETE....)"""
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method not in ['GET', 'HEAD', 'OPTIONS', 'TRASE']:
+            return csrf_protect(super().dispatch)
