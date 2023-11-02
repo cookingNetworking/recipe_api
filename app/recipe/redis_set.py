972ec37a2335f5ef9a8da05ecebf55b9f06f700c
as2229181 <as2229181@gmail.com>
2023-11-02 21:00:18 +0800

    nothing

--- app/recipe/redis_set.py
+++ app/recipe/redis_set.py
@@ -29,28 +29,28 @@ class RedisHandler:
         timeout = random.randint(1500, 1800)
         self.redis_client.set(f'{self.hkey_name}_{recipe_id}',data, timeout=timeout)
 
-    def get_recipe(self, recipe_id):
+    def get_recipe(self, recipe_id: int):
         """Get recipe in redis."""
         data = self.redis_client.get(f'Recipe_{recipe_id}')
         if data:
             return data.decode('utf-8')
         return None
 
-    def delete_recipe_in_cache(self, recipe_id):
+    def delete_recipe_in_cache(self, recipe_id: int):
         """Delete recipe in redis!"""
         self.redis_client.delete(f'Recipe_{recipe_id}')
 
-    def set_hkey(self, recipe_id, initinal_value=0):
+    def set_hkey(self, recipe_id: int, initinal_value=0):
         """Set hkey data type in cache(redis)."""
         self.redis_client.hset(f"{self.hkey_name}", f"{recipe_id}", initinal_value)
 
-    def get_hkey(self, recipe_id):
+    def get_hkey(self, recipe_id: int):
         """Get views value of giving recipe id."""
         recipe_view = self.redis_client.hget(f'{self.hkey_name}', f"{recipe_id}")
         if recipe_view:
             return int(recipe_view)
         return None
-    def increase_recipe_view(self, recipe_id, increment_value=1):
+    def increase_recipe_view(self, recipe_id: int, increment_value=1):
         """Increase value for hkey value"""
         self.redis_client.hincrby(f'{self.hkey_name}', f"{recipe_id}", increment_value)
 
