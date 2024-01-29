cd15affed70682b0800e15aab738435ab7f63784
as2229181 <as2229181@gmail.com>
2024-01-29 21:56:45 +0800

    fix setting

--- app/app/settings.py
+++ app/app/settings.py
@@ -51,9 +51,6 @@ INSTALLED_APPS = [
     'django_extensions',
     'storages',
     "rest_framework",
-    'oauth2_provider',
-    'social_django',
-    'rest_framework_social_oauth2',
     #project app
     "core",
     'recipe',
@@ -250,82 +247,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
-
-AUTHENTICATION_BACKENDS = (
-    #First user google oauth2 to login, if not, use the default authentication backend
-    'social_core.backends.google.GoogleOAuth2',
-   'django.contrib.auth.backends.ModelBackend',
-)
-SOCIAL_AUTH_URL_NAMESPACE = 'social'
-# django-rest-framework-social-oauth2 settings
-DRFSO2_PROPRIETARY_BACKEND_NAME = 'google-oauth2'
-
-DRFSO2_URL_NAMESPACE = 'oauth'
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET= os.environ.get("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
-GOOGLE_OAUTH2_REDIRECT_URI = 'http://localhost:80/social/complete/google-oauth2/'
-
-LOGIN_URL = 'user:login'
-
-LOGIN_REDIRECT_URL ='/'
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile',
-]
-
-
-# Python social oauth pipline
-
-SOCIAL_AUTH_PIPELINE = (
-    # Get the information we can about the user and return it in a simple
-    # format to create the user instance later. In some cases the details are
-    # already part of the auth response from the provider, but sometimes this
-    # could hit a provider API.
-    'social_core.pipeline.social_auth.social_details',
-
-    # Get the social uid from whichever service we're authing thru. The uid is
-    # the unique identifier of the given user in the provider.
-    'social_core.pipeline.social_auth.social_uid',
-
-    # Verifies that the current auth process is valid within the current
-    # project, this is where emails and domains whitelists are applied (if
-    # defined).
-    'social_core.pipeline.social_auth.auth_allowed',
-
-    # Checks if the current social-account is already associated in the site.
-    'social_core.pipeline.social_auth.social_user',
-
-    # Make up a username for this person, appends a random string at the end if
-    # there's any collision.
-    'social_core.pipeline.user.get_username',
-
-    # Send a validation email to the user to verify its email address.
-    # Disabled by default.
-    # 'social_core.pipeline.mail.mail_validation',
-
-    # Associates the current social details with another user account with
-    # a similar email address. Disabled by default.
-    # 'social_core.pipeline.social_auth.associate_by_email',
-
-    # Create a user account if we haven't found one yet.
-    'social_core.pipeline.user.create_user',
-
-    # Active social user instance.
-
-    'app.custom_pipline.set_user_active',
-
-    # Create the record that associates the social account with the user.
-    'social_core.pipeline.social_auth.associate_user',
-
-    # Populate the extra_data field in the social record with the values
-    # specified by settings (and the default ones like access_token, etc).
-    'social_core.pipeline.social_auth.load_extra_data',
-
-    # Update the user record with any changed info from the auth service.
-    'social_core.pipeline.user.user_details',
-)
 
 
 
