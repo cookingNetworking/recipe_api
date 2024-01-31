b9e8dae05fbad39a86669f346dc038908a02d092
as2229181 <as2229181@gmail.com>
2024-01-31 15:15:13 +0800

    SocialAuthException add.

--- app/app/custom_middleware.py
+++ app/app/custom_middleware.py
@@ -1,13 +1,19 @@
 """
 Customerize Middle ware!
 """
+from django.utils.deprecation import MiddlewareMixin
 from social_core.exceptions import AuthStateMissing
 from django.shortcuts import redirect
 
-class SocialAuthException:
+class SocialAuthException(MiddlewareMixin):
     """
     Hnadle state missing problem!
     """
     def process_exception(self, request, exception):
         if isinstance(exception, AuthStateMissing):
+            user = request.user
+            if user.is_authenticated:
+                #If the session id exist redirect to the path you set.
+                return redirect('swagger-ui')
+                #If the session id not exist , redirect google oauth page.
             return redirect('social:begin', backend='google-oauth2')
