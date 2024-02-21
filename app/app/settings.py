14d72c804ced0afa5fe78bc9ec3818240f95be5e
EC2 Default User <ec2-user@ip-172-31-9-241.ap-southeast-2.compute.internal>
2024-02-21 07:54:32 +0000

    for test

--- app/app/settings.py
+++ app/app/settings.py
@@ -376,24 +376,34 @@ AWS_CLOUDFRONT_KEY = env.str('AWS_CLOUDFRONT_KEY', multiline=True).encode('ascii
 
 #csrf cookies settings
 
-CSRF_COOKIE_SECURE = False
-
-SESSION_COOKIE_SECURE = False
 
 CSRF_COOKIE_HTTPONLY = True
 
+
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
-CSRF_TRUSTED_ORIGINS = ['https://www.cookingnetwork.co']
+CSRF_TRUSTED_ORIGINS = ['https://www.cookingnetwork.co',
+                        'http://localhost:3000',
+                        'http://127.0.0.1:3000'
+                        ]
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000',
+                        'http://127.0.0.1:3000',
+                        'https://cookingnetwork.co'
+                        ]
 
 if os.environ.get('DEV_ENV') == 'true':
-    SESSION_COOKIE_DOMAIN = 'localhost'
+    SESSION_COOKIE_DOAMIN = 'localhost'
     CSRF_COOKIE_DOMAIN = 'localhost'
+    CSRF_COOKIE_SECURE = False
+    SESSION_COOKIE_SECURE = False
 else:
     SESSION_COOKIE_DOMAIN = '.cookingnetwork.co'
     CSRF_COOKIE_DOMAIN = '.cookingnetwork.co'
-
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SAMESITE = 'None'
 
 CORS_ALLOW_METHODS = [
             'GET',
